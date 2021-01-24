06c12a5b52a27bcbd4a24ebe328ef83ea0140499
sipsuli <silja.angervuori@gmail.com>
2021-01-24 23:11:17 +0200

    tehtävä 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const raiseMenu = document.querySelector('.raiseMenu');
 const priseMenu = document.querySelector('.priseMenu');
 const fazerMenu = document.querySelector('.fazerMenu');
 
-import FazerMenu from "./fazer-week-example.json";// importataan paikallisessa hakemistossa oleva json tiedosto
+import FazerMenu from "./fazer-week-example.json";
 
 const menuList = [
   { name: 'Lingonberry jam', price: 4.00 },
@@ -16,21 +16,16 @@ const menuList = [
   { name: 'Pureed root vegetable soup with smoked cheese', price: 8.00 }
 ];
 
-//Näytä menun sisältö kokonaan tai per ID
 function showMenuId() {
   var id = Number(menuItem.value) - 1;
 
   if (id >= menuList.length || id <= -1) {
 
-
-    //document.getElementById("box").innerHTML = menuList.map(menuList => menuList);
     document.getElementById("box").innerHTML = menuList.map(select);
 
-    function select(value) {
+    function select(value, index, array) {
       return value.name;
     }
-
-
   } else {
     var meal = menuList[id].name;
     document.getElementById("box").innerHTML = meal;
@@ -44,16 +39,21 @@ function showMenuId() {
 }
 showMenu.addEventListener('click', showMenuId);
 
-// Järjestä hinnan mukaan (tässä on vain hinnat = pitäisi saada myös sisällöt mukaan)
+
+// Järjestä hinnan mukaan
 function sortByPrise() {
 
-  var menuListPrise = menuList.map(select);
-  document.getElementById("box").innerHTML = menuListPrise.sort();
+document.getElementById("box").innerHTML = "";
+let sortedMenu = menuList.sort((b, a) => {
+  return b.price - a.price;
+});
+document.getElementById("box").innerHTML = sortedMenu.map(select);
 
-  function select(value, index, array) {
-    return value.price;
-  }
+function select(value, index, array) {
+return value.name;
+ }
 }
+
 orderMenu.addEventListener('click', sortByPrise);
 
 //Laskee summan
@@ -75,17 +75,9 @@ priseMenu.addEventListener('click', sumMenu);
 
 
 function fazer() {
-//window.alert(FazerMenu.LunchMenus[0].DayOfWeek);
 
-  var fazerMenuListLength = FazerMenu.LunchMenus[0].SetMenus.length; //json importattu jo alussa
+  var fazerMenuListLength = FazerMenu.LunchMenus[0].SetMenus.length;
   document.getElementById("box").innerHTML = "";
-
-  //    var x;
-  //   for (x in fazerMenuList){
-  //   document.getElementById("box").innerHTML += fazerMenuList[x] + "<br>";
-  //   }
-  // }
-
 
   for (var i = 0; i < fazerMenuListLength; i++) {
 
@@ -98,7 +90,6 @@ function fazer() {
       return dish;
     }
   }
-
 }
 fazerMenu.addEventListener('click', fazer);
 
